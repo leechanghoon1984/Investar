78dfa1858c38c9fb43f723a50849ee7815dc41f5
leechangehoon1984 <conan1984@naver.com>
2021-10-27 14:20:27 +0900

    20201027 1419

--- Investar/urls.py
+++ Investar/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-#from hello import views
+from hello import views
 from index import views as index_views
 from django.urls import path, re_path
 
