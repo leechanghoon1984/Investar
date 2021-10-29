0527e536b66394f3a3abbbba3d01f5695a215eab
leechangehoon1984 <conan1984@naver.com>
2021-10-29 14:40:24 +0900

    20211029 1440

--- balance/views.py
+++ balance/views.py
@@ -31,7 +31,7 @@ def main_view(request):
         stock_count = format(int(x[1][0]), ',')
         sum = int(price) * int(x[1][0])
         stock_sum = format(sum, ',')
-        rows.appand([stock_name, x[0], cur_price, stock_count, cur_rate, stock_sum])
+        rows.append([stock_name, x[0], cur_price, stock_count, cur_rate, stock_sum])
         total = total + int(price) * int([1][0])
     
     total_amount = format(total, ',')
